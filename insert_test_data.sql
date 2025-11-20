168a582d5e0872b48d4bfecbeb1e5f70aabec3cb
Stockholmalion <stockholmalion@gmail.com>
2025-11-20 22:22:58 +0000

    fixed

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -4,7 +4,7 @@ USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
 
-INSERT INTO users(username, fistname, lastname, email, hashedPassword)
+INSERT INTO users(username, firstname, lastname, email, hashedPassword)
 VALUES (
     'gold',
     'gold',
