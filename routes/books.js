512b28291dbbb5a2c0d07d3b2321e19183a87856
Stockholmalion <stockholmalion@gmail.com>
2025-11-26 17:16:23 +0000

    return

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
-      res.redirect('users/login') // redirect to the login page
+      res.redirect('/users/login') // redirect to the login page
     } else { 
         next (); // move to the next middleware function
     } 
